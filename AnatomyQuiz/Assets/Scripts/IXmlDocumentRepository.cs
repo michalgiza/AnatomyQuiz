0c0edbc5c8a440a91cfee3694962f501d35dd662
karolina.krzykawska <karolina.krzykawska@o2.pl>
2018-06-10 12:20:14 +0200

    Dodanie wzorca projektowego Repository i DataObject

---
+++ AnatomyQuiz/Assets/Scripts/IXmlDocumentRepository.cs
@@ -0,0 +1,11 @@
+﻿
+using System.Xml;
+using System.Xml.Linq;
+
+
+public interface IXmlDocumentRepository
+{
+    void Save(string path, XmlDocument xdoc);
+    XmlDocument LoadXmlDocument(string path);
+    XDocument LoadXml(string path);
+}
--- AnatomyQuiz/Assets/Scripts/Objects/QuestionList.cs
+++ AnatomyQuiz/Assets/Scripts/Objects/QuestionList.cs
@@ -9,57 +9,20 @@ using System.Xml;
 public class QuestionList : MonoBehaviour
 {
     public Question[] questions;
+    public IXmlDocumentDataObject xmlDocumetnDataObject;
     // Use this for initialization
+
+     
     void Start ()
     {
         string path = @"FileXML\questions.xml";
-        
-        ReadXmlFile(path);
+        xmlDocumetnDataObject = new XmlDocumetnDataObject();
+        questions=xmlDocumetnDataObject.GetAllQuestions(path);
         Singleton.QuizManager.questions = this.questions;
 	}
 
     //
-    private void ReadXmlFile(string path)
-    {
-
-
-        XDocument doc = XDocument.Load(Path.GetFullPath(path));
-        var questionsArray = doc.Descendants("Question").Select(x => new Question
-        {
-            question = x.Element("Tresc").Value,
-            answerA = x.Element("AnswerA").Value,
-            answerB = x.Element("AnswerB").Value,
-            answerC = x.Element("AnswerC").Value,
-            answerD = x.Element("AnswerD").Value,
-            correctAnswerSign = x.Element("PoprawnaOdpowiedz").Value
-
-        }).ToArray();
-        StringToEnum(questionsArray);
-        questions = questionsArray;
-
-    }
-
-    //
-    private void StringToEnum(Question [] questionsArray)
-    {
-        for (int i = 0; i < questionsArray.Length; i++)
-        {
-            switch (questionsArray[i].correctAnswerSign)
-            {
-                case "A":
-                    questionsArray[i].correctAnswer = Question.PossibleAnswer.A;
-                    break;
-                case "B":
-                    questionsArray[i].correctAnswer = Question.PossibleAnswer.B;
-                    break;
-                case "C":
-                    questionsArray[i].correctAnswer = Question.PossibleAnswer.C;
-                    break;
-                case "D":
-                    questionsArray[i].correctAnswer = Question.PossibleAnswer.D;
-                    break;
-            }
-        }
-    }
+    
+    
     }
 
